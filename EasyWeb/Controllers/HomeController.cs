6d5bdabb374cdb2e1906f7a62bba64f94b869218
大石头 <nnhy@vip.qq.com>
2024-05-20 10:04:50 +0800

    目录优先，然后按照名称排序

--- EasyWeb/Controllers/HomeController.cs
+++ EasyWeb/Controllers/HomeController.cs
@@ -14,8 +14,10 @@ public class HomeController : ControllerBaseX
         var list = FileStorage.FindAllWithCache().FirstOrDefault(e => e.Enable);
         var entris = FileEntry.FindAllByStorageIdAndParentId(list.Id, 0).Where(e => e.Enable).ToList();
 
-        PageSetting.EnableNavbar = false;
+        // 目录优先，然后按照名称排序
+        entris = entris.OrderByDescending(e => e.IsDirectory).ThenBy(e => e.Name).ToList();
 
+        PageSetting.EnableNavbar = false;
         ViewBag.Title = "文件";
 
         return View(entris);
